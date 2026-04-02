045cf5b400ed46ca8df4e0d1aea0ca03ecdb9b40
sanzhar-developer <thesanjickplay@gmail.com>
2026-04-03 02:12:52 +0500

    исправак ошибок в сервере

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const authRoutes = require('./routes/authroutes');
+const authRoutes = require('./routes/authRoutes');
 const questionRoutes = require('./routes/questionRoutes');
 const testRoutes = require('./routes/testRoutes');
 const attemptRoutes = require('./routes/attemptRoutes');
